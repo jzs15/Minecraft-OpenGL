92b7992cb84dba9072ce8297b1f7dda098f9075b
jzs15 <wlstjr214@naver.com>
2018-11-24 19:20:38 +0800

    Change window name

--- Minecraft/main.cpp
+++ Minecraft/main.cpp
@@ -50,7 +50,7 @@ int main()
 
 	// glfw window creation
 	// --------------------
-	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Minecraft", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
